2a9ae464046334eee6ecd7d015983c9b34d641dc
Ismoil <muhiddinovismoil2@gmail.com>
2024-11-27 00:25:02 +0500

    homework is added successfully and route of it is also finished with all validations

--- src/routes/homework.routes.js
+++ src/routes/homework.routes.js
@@ -9,8 +9,24 @@ import {
 } from "../controllers/index.js";
 import { homeworkSchema } from "../validations/index.js";
 export const homeworksRouter = Router();
-homeworksRouter.get("/", getAllHomeworksCon);
-homeworksRouter.get("/:id", getHomeworkByIdCon);
-homeworksRouter.post("/", validateData(homeworkSchema), createHomeworkCon);
-homeworksRouter.put("/:id", updateHomeworkByIdCon);
-homeworksRouter.delete("/:id", deleteHomeworkByIdCon);
+homeworksRouter.get("/", authGuard, getAllHomeworksCon);
+homeworksRouter.get("/:id", authGuard, getHomeworkByIdCon);
+homeworksRouter.post(
+    "/",
+    authGuard,
+    roleGuard("student"),
+    validateData(homeworkSchema),
+    createHomeworkCon
+);
+homeworksRouter.put(
+    "/:id",
+    authGuard,
+    roleGuard("student"),
+    updateHomeworkByIdCon
+);
+homeworksRouter.delete(
+    "/:id",
+    authGuard,
+    roleGuard("teacher"),
+    deleteHomeworkByIdCon
+);
